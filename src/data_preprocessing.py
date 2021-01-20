9522990c4cb7ff608d3675f92110b28b32eb5612
Arastal <bergeron.florian@gmail.com>
2021-01-20 22:12:30 +0100

    [UP] - Clean code.

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -13,7 +13,7 @@ STOPSIGN_TRAIN_IMG = DATA_DIR + "stop_sign/train/"
 def rename_label(path_label):
     print("[!] - Processing renaming label...")
     
-    for txt_file in glob.glob(path_label + '/*.txt'):
+    for txt_file in glob.glob(path_label + "/*.txt"):
         f = open(txt_file, "r+")
         txt = f.read().replace("p s", "p_s")
         txt = txt.lower()
@@ -28,29 +28,27 @@ def resize_bounding_box(path_label, path_image):
     print("\n[!] - Processing resizing BBox...")
 
     for element in os.listdir(path_label):
-        a = str(element)
-        a = a.replace('txt','jpg')
-        #print(a)
-        img = cv2.imread(path_image+'/'+a, cv2.IMREAD_UNCHANGED)
-        width = int(img.shape[1])
+        list_content = []
+
+        filename = str(element)
+        filename = filename.replace("txt", "jpg")
+        img = cv2.imread(path_image + "/" + filename, cv2.IMREAD_UNCHANGED)
         height = int(img.shape[0])
-        file = open(path_label+"/"+element, "r+")
+        width = int(img.shape[1])
+        file = open(path_label + "/" + element, "r+")
         content = file.readlines()
         str_content = str(content[0])
-        list_content = []
         list_content = str_content.split()
-        #print(list_content)
-        classe = list_content[0]
+                classe = list_content[0]
         hitbox_x_min = float(list_content[1])
         hitbox_x_max = float(list_content[2])
         hitbox_y_min = float(list_content[3])
         hitbox_y_max = float(list_content[4])
-        hitbox_x_min = hitbox_x_min/width *500
-        hitbox_x_max = hitbox_x_max/width *500
-        hitbox_y_min = hitbox_y_min/height *500
-        hitbox_y_max = hitbox_y_max/height *500
-        string  = (classe+ ' '+str(hitbox_x_min)+ ' '+str(hitbox_x_max)+' '+str(hitbox_y_min)+' '+str(hitbox_y_max)+' ')
-        #print(string)
+        hitbox_x_min = hitbox_x_min / width * 500
+        hitbox_x_max = hitbox_x_max / width * 500
+        hitbox_y_min = hitbox_y_min / height * 500
+        hitbox_y_max = hitbox_y_max / height * 500
+        string  = (classe + " " + str(hitbox_x_min) + " " + str(hitbox_x_max) + " " + str(hitbox_y_min) + " " + str(hitbox_y_max) + " ")
         file.seek(0)
         file.write(string)
         file.truncate()
@@ -63,14 +61,13 @@ def resize_image(path_image):
     print("\n[!] - Processing resizing images...")
 
     for element in os.listdir(path_image):
-        if element.endswith('.jpg') or element.endswith('.jpeg') or element.endswith('png'):
-            img = cv2.imread(path_image+'/'+element, cv2.IMREAD_UNCHANGED)
-            #print('Original Dimensions : ',img.shape)
-            width = int(img.shape[1])
+        if element.endswith(".jpg") or element.endswith(".jpeg") or element.endswith("png"):
+            img = cv2.imread(path_image + "/" + element, cv2.IMREAD_UNCHANGED)
             height = int(img.shape[0])
-            dim = (500, 500)
+            width = int(img.shape[1])
+            dim = (height, width)
             resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-            cv2.imwrite(path_image+'/'+element, resized)
+            cv2.imwrite(path_image + "/" + element, resized)
     
     print("[!] - All images have been resized!")
     
@@ -91,16 +88,13 @@ def txt_to_csv(path_image, path_label):
 
     txt_list = []
 
-    for txt_file in glob.glob(path_label + '/*.txt'):
+    for txt_file in glob.glob(path_label + "/*.txt"):
         f = open(txt_file, "r")
         txt = f.readlines()
         txt = str(txt[0]).split()
-
         txt_file = txt_file.split("\\")
         txt_file = txt_file[-1].replace("txt", "jpg")
-
-        img = cv2.imread(path_image+'/'+txt_file, cv2.IMREAD_UNCHANGED)
-
+        img = cv2.imread(path_image + "/" + txt_file, cv2.IMREAD_UNCHANGED)
         filename = txt_file
         label = txt[0] 
         width = int(img.shape[1])
@@ -119,11 +113,9 @@ def txt_to_csv(path_image, path_label):
                     x_max,
                     y_min,
                     y_max)
-            # print(data)
             txt_list.append(data)
-            # print(txt_list)
 
-    column_name = ['filename', 'label', 'width', 'height', 'xmin', 'ymin', 'xmax', 'ymax']
+    column_name = ["filename", "label", "width", "height", "xmin", "ymin", "xmax", "ymax"]
     txt_df = pd.DataFrame(txt_list, columns=column_name)
     txt_df = txt_df.drop_duplicates()
     txt_df = txt_df.reset_index(drop=True)
@@ -132,12 +124,9 @@ def txt_to_csv(path_image, path_label):
 
     return txt_df
 
-# rename_label(STOPSIGN_TRAIN_LABEL)
-# resize_bounding_box(STOPSIGN_TRAIN_LABEL, STOPSIGN_TRAIN_IMG)
-# resize_image(STOPSIGN_TRAIN_IMG)
+rename_label(STOPSIGN_TRAIN_LABEL)
+resize_bounding_box(STOPSIGN_TRAIN_LABEL, STOPSIGN_TRAIN_IMG)
+resize_image(STOPSIGN_TRAIN_IMG)
 stopSign_df = txt_to_csv(STOPSIGN_TRAIN_IMG, STOPSIGN_TRAIN_LABEL)
 
 print(stopSign_df)
-print(stopSign_df.head(40))
-
-
